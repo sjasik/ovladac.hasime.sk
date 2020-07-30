1d1082e8551129cbbb1d51d56f2fe952f4a0c8cc
sjasik <55330564+sjasik@users.noreply.github.com>
2020-07-30 13:13:49 +0200

    Ovládač pre hasime.sk
    umožnuje vyhlásiť poplach na diaľku

---
+++ precache-manifest.c160f5a00a22f7143c5743b1eeb12553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c460bea3b5449fcfe069",
+    "url": "/css/app.ba89571a.css"
+  },
+  {
+    "revision": "382db81aee5e53b16b98",
+    "url": "/css/chunk-vendors.2e17820a.css"
+  },
+  {
+    "revision": "3e993bc14badfd21787d2257df13684a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c460bea3b5449fcfe069",
+    "url": "/js/app.2abd2738.js"
+  },
+  {
+    "revision": "382db81aee5e53b16b98",
+    "url": "/js/chunk-vendors.d1c2b0c0.js"
+  },
+  {
+    "revision": "df93e6906c33a0e85501cfe34530ed43",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
